4bbf008fde428f77800ff796ebc7f51396ef0e82
Amr Eid <90511895+Amreid011@users.noreply.github.com>
2022-03-26 16:39:52 +0200

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -16,29 +16,34 @@ using namespace std;
 
 int main(){
     int x,z,y;
+
     cout<<"plz choize \n(1)addition\n(2)subtraction \n(3)multiplication \n(4)division\n ";
     cin>>x;
-    if(x>0 && x<5){
-        cout<<"plz Entur 2 number :  ";
-        cin>>z>>y;   
-        if(x==1){
-            cout<< x <<" + "<< y <<" = "<< x+y <<endl; ;    
+    do{
+        if(x>0 && x<5){
+            cout<<"plz Entur 2 number :  ";
+            cin>>z>>y;   
+            if(x==1){
+                cout<< z <<" + "<< y <<" = "<< z+y <<endl; ;    
+            }
+            else if(x==2){
+                cout<< z <<" - "<< y <<" = "<< z-y <<endl;    
+            }
+            else if(x==3){
+                cout<< z <<" * "<< y <<" = "<<(long) z*y <<endl; 
+            }
+            else if(x==4){
+                if(y==0)
+                    cout<<"sorry , Divide by zero is not correct";
+                else
+                    cout<< z <<" / "<< y <<" = "<<(double) z/y <<endl;    
+            }
         }
-        else if(x==2){
-            cout<< x <<" - "<< y <<" = "<< x-y <<endl;    
+        else {
+            cout<<"plz Entur number betwen 1 to 4 .\n";
         }
-        else if(x==3){
-            cout<< x <<" * "<< y <<" = "<<(long) x*y <<endl; 
-        }
-        else if(x==4){
-            if(y==0)
-                cout<<"sorry , Divide by zero is not correct";
-            else
-                cout<< x <<" / "<< y <<" = "<<(double) x/y <<endl;    
-        }
-    }
-    else {
-        cout<<"plz Entur number betwen 1 to 4 .";
-    }
+        cout<<"\nplz choize \n(1)addition\n(2)subtraction \n(3)multiplication \n(4)division\n(-1)exit\n ";
+        cin>>x;
+    } while (x!=-1);
     return 0;
 }
